b29ff648ad239f23be683f8d3a6b8b0d9bd0e426
dalgyd <79942372+dalgyd@users.noreply.github.com>
2021-11-06 22:21:35 -0500

    login de pacientes y terapeutas

--- app/src/main/java/com/example/fisiomovil/IngresoPaciente.java
+++ app/src/main/java/com/example/fisiomovil/IngresoPaciente.java
@@ -3,6 +3,11 @@ package com.example.fisiomovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class IngresoPaciente extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class IngresoPaciente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_paciente);
+
+        getSupportActionBar().hide();
+
+        TextView usernamePaciente = (TextView) findViewById(R.id.usernamePaciente);
+        TextView passwordPaciente = (TextView) findViewById(R.id.passwordPaciente);
+
+        MaterialButton loginbtnP = (MaterialButton) findViewById(R.id.loginbtnP);
+
+        //Codigo del boton tipo login
+        loginbtnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (usernamePaciente.getText().toString().equals("admin") && passwordPaciente.getText().toString().equals("admin")){
+                    Toast.makeText(IngresoPaciente.this,"Ingreso Exitoso", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(IngresoPaciente.this,"Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();}
+            }
+        });
     }
 }
