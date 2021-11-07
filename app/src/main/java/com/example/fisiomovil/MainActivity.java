9e3f86a20cc9eba330ddc7fcb6f920a0295df9f9
dalgyd <79942372+dalgyd@users.noreply.github.com>
2021-11-06 20:02:10 -0500

    Creacion registro e ingreso, pte formularios

--- app/src/main/java/com/example/fisiomovil/MainActivity.java
+++ app/src/main/java/com/example/fisiomovil/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.fisiomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();//Oculta barra superior
+    }
+
+    //Cambia la pantalla a el ingreso como paciente
+    public void ingresoPaciente (View p){
+        Intent cambiar = new Intent(this, IngresoPaciente.class);
+        startActivity(cambiar);
+    }
+
+    //Cambia la pantalla a el ingreso como terapueta
+    public void ingresoTerapeuta (View t){
+        Intent cambiar2 = new Intent(this, IngresoTerapeuta.class);
+        startActivity(cambiar2);
+    }
+
+    //Cambia la pantalla a el tipo de registro
+    public void registrarse (View r){
+        Intent cambiar3 = new Intent(this, TipoRegistro.class);
+        startActivity(cambiar3);
     }
 }
